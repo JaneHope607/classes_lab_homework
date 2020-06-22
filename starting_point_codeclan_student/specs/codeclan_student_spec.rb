2a7641b91597225981cbd89a7980ee9ad19d41ec
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-06-22 14:15:02 +0100

    added specs codeclan_student

---
+++ starting_point_codeclan_student/specs/codeclan_student_spec.rb
@@ -0,0 +1,14 @@
+require('minitest/autorun')
+require('minitest/reporters')
+require_relative('../codeclan_student.rb')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new()
+
+class TestCodeclanStudent < Minitest::Test
+
+    def 
+    
+        
+    end
+
+
+end
